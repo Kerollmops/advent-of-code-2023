91b39d54e74ce9d0f70c8284854589a98996f354
Clément Renault <clement@meilisearch.com>
2023-12-03 18:20:27 +0100

    It finally works

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,5 +1,8 @@
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+
 use ndarray::Array2;
-use slice_group_by::StrGroupBy;
+use slice_group_by::{GroupBy, StrGroupBy};
 
 const INPUT: &str = include_str!("../input.txt");
 
@@ -18,8 +21,8 @@ fn main() -> anyhow::Result<()> {
     // We modify the array to alterate every number that must be considered.
     for (r, line) in INPUT.lines().enumerate() {
         for (c, x) in line.chars().enumerate() {
-            if CharType::from(x) == CharType::Symbol {
-                touch_square_around(&mut array, [r, c]);
+            if let CharType::Symbol(_) = CharType::from(x) {
+                touch_square_around(&mut array, [r, c], '.');
             }
         }
     }
@@ -40,7 +43,61 @@ fn main() -> anyhow::Result<()> {
     let answer: usize = numbers.iter().sum();
     println!("The first answer is: {answer}");
 
+    // ---- PART 2 ----
+
+    let rows = INPUT.lines().count();
+    let columns = INPUT.lines().next().unwrap().len();
+
+    // We generate the array conresponding to the input
+    let mut array = Array2::<char>::default((rows, columns));
+    for (r, line) in INPUT.lines().enumerate() {
+        for (c, x) in line.chars().enumerate() {
+            array[[r, c]] = x;
+        }
+    }
+
+    // We modify the array to alterate every number that must be considered.
+    let mut id = 'A'..;
+    for (r, line) in INPUT.lines().enumerate() {
+        for (c, x) in line.chars().enumerate() {
+            if let CharType::Symbol('*') = CharType::from(x) {
+                if symbol_touches_exactly_two_digits(&array, [r, c]) {
+                    let id = id.next().unwrap();
+                    touch_square_around(&mut array, [r, c], id);
+                }
+            }
+        }
+    }
+
+    // Then we iterate on both strings at the same time, the original one and the modified one.
+    // Eevrytime the extracted number string is different between both chunks
+    // it means that it was near a symbol and we need to take it.
+    let touched_array = ndarray_into_string(&array);
+
+    let mut numbers = HashMap::new();
+    for (chunk, touched_chunk) in INPUT.lines().zip(touched_array.lines()) {
+        let iter = parallel_iter(chunk, touched_chunk)
+            .filter(|(original, _)| CharType::from(original.chars().next().unwrap()).is_number())
+            .filter_map(|(original, touched)| char_diff(original, touched).map(|c| (original, c)))
+            .map(|(original, c)| (c, original.parse::<usize>().unwrap()));
+        for (c, n) in iter {
+            match numbers.entry(c) {
+                Entry::Occupied(mut entry) => *entry.get_mut() *= n,
+                Entry::Vacant(entry) => {
+                    entry.insert(n);
+                }
+            }
+        }
+    }
+
+    let answer: usize = numbers.values().copied().sum();
+    println!("The second answer is: {answer}");
+
     Ok(())
+}
+
+fn char_diff(original: &str, touched: &str) -> Option<char> {
+    original.chars().zip(touched.chars()).find_map(|(o, t)| (o != t).then_some(t))
 }
 
 fn parallel_iter<'a>(
@@ -48,7 +105,8 @@ fn parallel_iter<'a>(
     mut copy: &'a str,
 ) -> impl Iterator<Item = (&'a str, &'a str)> + 'a {
     input.linear_group_by_key(CharType::from).map(move |chunk| {
-        let (left, right) = copy.split_at(chunk.len());
+        let (i, c) = copy.char_indices().take(chunk.len()).last().unwrap();
+        let (left, right) = copy.split_at(i + c.len_utf8());
         copy = right;
         (chunk, left)
     })
@@ -65,7 +123,38 @@ fn ndarray_into_string(array: &Array2<char>) -> String {
     touched_input
 }
 
-fn touch_square_around(array: &mut Array2<char>, [r, c]: [usize; 2]) {
+fn symbol_touches_exactly_two_digits(array: &Array2<char>, [r, c]: [usize; 2]) -> bool {
+    #[rustfmt::skip]
+    let ops: [[isize; 2]; 9] = [
+        [-1,-1], [-1, 0], [-1, 1],
+        [ 0,-1], [ 0, 0], [ 0, 1],
+        [ 1,-1], [ 1, 0], [ 1, 1],
+    ];
+
+    let is_digit = |[ro, co]: &[isize; 2]| {
+        let r: usize = match (r as isize + ro).try_into().ok() {
+            Some(r) => r,
+            None => return false,
+        };
+        let c: usize = match (c as isize + co).try_into().ok() {
+            Some(c) => c,
+            None => return false,
+        };
+        let value = array.get([r, c]).copied().map(CharType::from);
+        value == Some(CharType::Digit)
+    };
+
+    let mut matches: Vec<_> = ops.iter().map(is_digit).collect();
+    // inserts falses to split lanes
+    matches.insert(3, false);
+    matches.insert(7, false);
+
+    let groups = matches.linear_group_by_key(|b| *b).filter(|s| s[0]).count();
+
+    groups == 2
+}
+
+fn touch_square_around(array: &mut Array2<char>, [r, c]: [usize; 2], v: char) {
     #[rustfmt::skip]
     let ops: [[isize; 2]; 9] = [
         [-1,-1], [-1, 0], [-1, 1],
@@ -80,7 +169,7 @@ fn touch_square_around(array: &mut Array2<char>, [r, c]: [usize; 2]) {
         })
         .for_each(|[r, c]| {
             if let Some(x) = array.get_mut([r, c]) {
-                *x = '.';
+                *x = v;
             }
         });
 }
@@ -88,7 +177,7 @@ fn touch_square_around(array: &mut Array2<char>, [r, c]: [usize; 2]) {
 #[derive(Debug, PartialEq)]
 enum CharType {
     Digit,
-    Symbol,
+    Symbol(char),
     Dot,
 }
 
@@ -103,7 +192,7 @@ impl From<char> for CharType {
         match c {
             '.' => CharType::Dot,
             '0'..='9' => CharType::Digit,
-            _ => CharType::Symbol,
+            c => CharType::Symbol(c),
         }
     }
 }
